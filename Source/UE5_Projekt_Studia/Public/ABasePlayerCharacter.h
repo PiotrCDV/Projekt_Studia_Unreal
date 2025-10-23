e9b26803650b0c8b71bdf84b06f58b76fb5e70b3
PiotrCodi <151371404+PiotrCDV@users.noreply.github.com>
2025-10-23 19:59:16 +0200

    Input mapping controller

--- Source/UE5_Projekt_Studia/Public/ABasePlayerCharacter.h
+++ Source/UE5_Projekt_Studia/Public/ABasePlayerCharacter.h
@@ -4,14 +4,28 @@
 
 #include "CoreMinimal.h"
 #include "ABaseCharacter.h"
+#include "InputMappingContext.h"
 #include "ABasePlayerCharacter.generated.h"
 
 /**
  * 
  */
+class InputMappingContext;
+class UInputAction;
+
 UCLASS()
 class UE5_PROJEKT_STUDIA_API AABasePlayerCharacter : public AABaseCharacter
 {
 	GENERATED_BODY()
+
+public:
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input)
+     UInputMappingContext* MappingContext;
+     UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Input)
+     UInputAction* MoveAction;
+     UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Input)
+     UInputAction* EquipAction;
+     UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Input)
+     UInputAction* AttackAction;
 	
 };
